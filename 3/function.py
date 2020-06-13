177740c2f243b62a305f8a8a466fe2dc0147ea49
Dan Ge <linan.lqq0@gmail.com>
2020-06-14 01:13:33 +1000

    function and class

---
+++ 3/function.py
@@ -0,0 +1,12 @@
+'''a = -1
+print(a)
+print(abs(a))
+
+b = 2
+print(max(a, b))'''
+
+def add(num1, num2, num3):
+    result = num1 + num2 + num3
+    return result
+
+print(add(1, 2, 3))
---
+++ 3/loop.py
@@ -0,0 +1,14 @@
+list = [1, 2, 3, 4, 5]
+
+'''for element in list:
+    if element == 4: 
+        continue
+    print(element)'''
+
+x = 1
+while x < 5:
+    if x == 3:
+        x = x + 1
+        continue
+    print(x)
+    x = x + 1
